2c83372a46c11bdc1030330814e6cb06232bb585
abayomi igwubor <igwubor@outlook.com>
2020-05-07 09:01:05 +0100

    Add project files.

---
+++ DelegateApp/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+namespace DelegateApp
+{
+    public delegate string strMyDel(string str);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestDelegate testDelegate = new TestDelegate();
+            //using generic delegates
+            Func<string, string> myTestDelegateSpace = testDelegate.Space;
+            Func<string, string> myTestDelegateReverse = testDelegate.Reverse;
+            string spacedString =  myTestDelegateSpace("CodebitsAcademy");
+            string reversedSting = myTestDelegateReverse("CodebitsAcademy");
+            Console.WriteLine(spacedString);
+            Console.WriteLine(reversedSting);
+            //using Non-genric delegates
+            strMyDel strMySpace = testDelegate.Space;
+            strMyDel strMyReverse = testDelegate.Reverse;
+           string nameSpaced = strMySpace.Invoke("abayomi");
+            string nameReversed = strMyReverse.Invoke("abayomi");
+
+            Console.WriteLine(nameSpaced);
+            Console.WriteLine(nameReversed);
+
+        }
+
+
+        class TestDelegate
+        {
+            public string Space(string name)
+            {
+                string strResult = String.Join(" ", name.Reverse());
+                string newSpace = "";
+                for(int i = strResult.Length-1; i >= 0; i--)
+                {
+                    newSpace += strResult[i];
+                }
+                return newSpace;
+            }
+
+            public string Reverse(string name)
+            {
+                char[] charArray = name.ToCharArray();
+                Array.Reverse(charArray);
+                return new string(charArray);
+              
+            }
+        }
+    }
+}
